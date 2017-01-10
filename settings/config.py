e45c82a723d4e2a63e2c6a0e6151361e3798b7ea
Tina Stith-Twine <tinatwine@gmail.com>
2017-01-10 15:51:38 -0500

    Updated changes

--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///appdb.db'
+SQLALCHEMY_DATABASE_URI = 'postgres://nptamlcuhfmlsd:584ffd5f48aa13a2541f90aab76699a50630554cea56e12b4cab8f664caee1e4@ec2-50-17-220-223.compute-1.amazonaws.com:5432/df0cr51h15gu64 '
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOAD_FOLDER = '/static/images'
